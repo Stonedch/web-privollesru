873b28232577064e98e61ce6e5723920e3b742ba
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-01-16 17:10:12 +0300

    feat: add robots.txt

--- vlknn/urls.py
+++ vlknn/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from .views import *
 
 urlpatterns = [
+    path('robots.txt/', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('', IndexView.as_view(), name='index'),
     path('feedback/', FeedbackView.as_view(), name='feedback'),
 ]
