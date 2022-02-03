261ec00430283307f7dc5191d467947b30ce6865
Pavel Klimov <klimov@194-67-108-17.cloudvps.regruhosting.ru>
2022-02-03 21:03:41 +0300

    404 cod site

--- web_privolles/web_privolles/urls.py
+++ web_privolles/web_privolles/urls.py
@@ -3,3 +3,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('privolles.urls'), name='index'),
 ]
+handler404 = "python3 ./manage.py migrateprivolles.views.page_not_found_view"
