8ecf70a33a9e46856becc8edca2d170730148d4b
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-02-06 00:22:28 +0300

    chore: setup postgres for wagtail

--- privollesru/settings/base.py
+++ privollesru/settings/base.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'privollesru.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.getenv('SQL_USER', 'user'),
+        'PASSWORD': os.getenv('SQL_PASSWORD', 'password'),
+        'HOST': os.getenv('SQL_HOST', 'localhost'),
+        'PORT': os.getenv('SQL_PORT', '5432'),
     }
 }
 
