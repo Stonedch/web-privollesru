784015a89134829a47f4b4b43ecb091a367d1b10
keshah239 <klimov@194-67-108-17.cloudvps.regruhosting.ru>
2022-02-05 14:34:42 +0300

    404

--- web_privolles/web_privolles/urls.py
+++ web_privolles/web_privolles/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('privolles.urls'), name='index'),
 ]
-handler404 = "python3 ./manage.py migrateprivolles.views.page_not_found_view"
+
+handler404 = 'web_privolles.views.page_not_found_view'
+
--- web_privolles/web_privolles/views.py
+++ web_privolles/web_privolles/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+
+
 def page_not_found_view(request, exception):
+    print('DEBUG: ', request)
     return render(request, '404.html', status=404)
+
