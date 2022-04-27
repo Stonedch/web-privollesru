5d343d510b04f2ddf8c3b61eb62a0e73bb4748e5
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-04-27 18:08:41 +0300

    feat[frontend]: initial Header component

--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Header } from 'components/Header';
 
 function HomeView() {
     return (
         <>
-            <h1>Home</h1>
+            <Header />
         </>
     );
 }
