5b0e64291a758a19284af824783ba32d023b9dbc
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-04-28 12:43:09 +0300

    feat[frontend]: add Brand component

---
+++ frontend/src/components/Brand/Brand.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Brand.module.scss';
+import logotype from 'assets/brand/logotype-white.svg';
+
+function Brand(props) {
+    const { href, alt } = props;
+
+    return (
+        <a className={styles.brand} href={href}>
+            <img src={logotype} alt={ alt ? alt : 'logotype' } />
+        </a>
+    );
+}
+
+export { Brand };
+
---
+++ frontend/src/components/Brand/index.js
@@ -0,0 +1,2 @@
+export { Brand } from './Brand';
+
