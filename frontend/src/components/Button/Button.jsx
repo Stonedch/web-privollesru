f8229877022d2af07c85b38a9abab1e923c6f95a
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-04-29 15:31:13 +0300

    feat[frontend]: add the Button component

---
+++ frontend/src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Button.module.scss';
+
+function Button(props) {
+    const { onClick, opacity, children } = props;
+
+    return (
+        <div
+            className={[styles.button, opacity ? styles.opacity : null].join(' ')}
+            onClick={onClick}
+        >
+            {children}
+        </div>
+    );
+}
+
+export { Button };
+
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Header.module.scss';
 import { Brand } from 'components/Brand';
 import { CompanyInfo } from 'components/CompanyInfo';
+import { Button } from 'components/Button';
 
 function Header() {
     return (
@@ -15,6 +16,11 @@ function Header() {
                     <h1 className={styles.title}>Приволжская лесная компания</h1>
                     <h2 className={styles.subtitle}>Поставляем качественный лес и пиломатериалы</h2>
                     <span className={styles.additional}>В любых количествах точно в срок по РФ и на экспорт.</span>
+                    <Button
+                        opacity={true}
+                    >
+                        Каталог продукции
+                    </Button>
                 </div>
             </div>
         </header>
