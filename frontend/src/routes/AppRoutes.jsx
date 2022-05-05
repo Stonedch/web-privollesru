2a1296d3f24eb0a1847feacba511775206f01d4f
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-05-05 17:14:49 +0300

    chore[frontend]: add privacy page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { HomeView } from 'views/HomeView';
+import { PrivacyView } from 'views/PrivacyView';
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path='/' element={<HomeView />} />
+            <Route path='/privacy/' element={<PrivacyView />} />
         </Routes>
     );
 }
 
 export { AppRoutes };
-
