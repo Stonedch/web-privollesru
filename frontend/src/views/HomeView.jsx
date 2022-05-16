49297f4d089e531445482071b763b1b86c39a9a4
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-05-16 16:37:51 +0300

    feat[frontend]: add our clients component

--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
 import { Benefits } from 'components/Benefits';
+import { Clients } from 'components/Clients';
 
 function HomeView() {
     return (
         <>
             <Header />
+            <Clients />
             <Benefits />
             <Footer />
         </>
