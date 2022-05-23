b9eb2f9148dac9786620e1d63f73b2a296489236
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-05-23 16:31:19 +0300

    feat[frontend]: add contacts and 404 pages

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { HomeView } from 'views/HomeView';
 import { PrivacyView } from 'views/PrivacyView';
+import { ContactsView } from 'views/ContactsView';
+import { NotFoundView } from 'views/NotFoundView';
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path='/' element={<HomeView />} />
             <Route path='/privacy/' element={<PrivacyView />} />
+            <Route path='/contacts/' element={<ContactsView />} />
+            <Route path='*' element={<NotFoundView />} />
         </Routes>
     );
 }
