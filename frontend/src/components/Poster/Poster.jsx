ad2433062c3e73e47074fb15066b01fbb658466d
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-05-23 18:41:21 +0300

    chore[frontend]: add modal form to poster

--- frontend/src/components/Poster/Poster.jsx
+++ frontend/src/components/Poster/Poster.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Poster.module.scss';
+import { ModalWindow } from 'components/ModalWindow';
+import { Form } from 'components/Form';
+import { Label } from 'components/Label';
+import { Input } from 'components/Input';
 import { Button } from 'components/Button';
 import logotype from 'assets/brand/mini.svg';
 import background from 'assets/backgrounds/poster.jpg';
 import avatar from 'assets/avatar.png';
 
 const Poster = () => {
+    const [isOpenWindow, setIsOpenWindow] = useState(false);
+
+    const toggleWindow = () => {
+        setIsOpenWindow(!isOpenWindow);
+    };
+
     return (
         <div className={`${styles.poster} ${styles.screen}`}>
             <div className={styles.background}>
@@ -33,9 +43,28 @@ const Poster = () => {
                     <p>и мы вышлем Вам прайс на интересующую Вас продукцию!</p>
                     <a href='tel: +79877575718'>+7 (987) 757-57-18</a>
                     <a href='tel: +79103866661'>+7 (910) 386-66-61</a>
-                    <Button opacity={true}>Заказать звонок</Button>
+                    <Button onClick={() => toggleWindow()} opacity={true}>
+                        Заказать звонок
+                    </Button>
                 </div>
             </div>
+            <ModalWindow isOpen={isOpenWindow} onClose={toggleWindow}>
+                <Form
+                    title='Мы заинтересованы в сотрудничестве с Вами!'
+                    subtitle='Оставьте заявку в наш Экспортный отдел! Узнайте стоимость доставки до Вашей страны!'
+                >
+                    <Label title='Ваше имя'>
+                        <Input type='text' name='name' placeholder='Иван' />
+                    </Label>
+                    <Label title='Введите номер телефона'>
+                        <Input
+                            type='tel'
+                            name='phone_number'
+                            placeholder='+7 (___) ___ __ __'
+                        />
+                    </Label>
+                </Form>
+            </ModalWindow>
         </div>
     );
 };
