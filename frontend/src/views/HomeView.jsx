1c4171940ba774cb625fb9afc8ad04503c900177
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-05-23 16:45:43 +0300

    feat[frontend]: add About Us component

--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -4,12 +4,14 @@ import { Footer } from 'components/Footer';
 import { Benefits } from 'components/Benefits';
 import { Clients } from 'components/Clients';
 import { Questions } from 'components/Questions';
+import { AboutUs } from 'components/AboutUs';
 
 function HomeView() {
     return (
         <>
             <Header />
             <Benefits />
+            <AboutUs />
             <Clients />
             <Questions />
             <Footer />
