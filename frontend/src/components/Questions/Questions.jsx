44f5656c57457ba00cc33f770d57823edacc1ce2
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-05-30 16:06:46 +0300

    feat[frontend]: api questions

--- frontend/src/components/Questions/Questions.jsx
+++ frontend/src/components/Questions/Questions.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Questions.module.scss';
 import { Title } from 'components/Title';
 import { Item } from './Item';
 
 function Questions() {
-    return (
+    const { REACT_APP_API_URL } = process.env;
+    const [questions, setQuestions] = useState(null);
+
+    const endpoint = REACT_APP_API_URL + 'questions/';
+
+    useEffect(() => {
+        fetch(endpoint)
+            .then((response) => response.json())
+            .then((response) => setQuestions(response.results));
+    });
+
+    return questions ? (
         <div className={`${styles.questions} ${styles.screen}`}>
             <div className={styles.content}>
                 <Title title='Помощь / Вопрос-ответ' />
-                <Item title='Как сделать заказ?'>
-                    Позвоните нам по телефону, указанному на сайте или отправьте
-                    заявку на интересующую Вас продукцию, и наш менеджер
-                    свяжется с вами течении 10 минут.
-                </Item>
-                <Item title='Как сделать заказ?'>
-                    Позвоните нам по телефону, указанному на сайте или отправьте
-                    заявку на интересующую Вас продукцию, и наш менеджер
-                    свяжется с вами течении 10 минут.
-                </Item>
+                {questions.map((question) => (
+                    <Item title={question.title}>{question.body}</Item>
+                ))}
             </div>
         </div>
-    );
+    ) : null;
 }
 
 export { Questions };
