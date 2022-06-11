4263bd9de490a2e0728cdd3500dd019f88ad833b
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-06-11 16:17:29 +0300

    feat[frontend]: add yandex metrika from db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppRoutes } from './routes/AppRoutes';
+import { YMInitializer } from 'react-yandex-metrika';
+import { useLocation } from 'react-router-dom';
 
 function App() {
-    return <AppRoutes />;
+    const { REACT_APP_API_URL } = process.env;
+    const [account, setAccount] = useState(null);
+    const location = useLocation();
+
+    useEffect(() => {
+        const endpoint = REACT_APP_API_URL + 'settings/';
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        };
+
+        fetch(endpoint, requestOptions)
+            .then((response) => response.json())
+            .then((response) => {
+                setAccount(response.yandex_metrika);
+            });
+    }, [location]);
+
+    return (
+        <>
+            {account ? (
+                <YMInitializer
+                    accounts={[account]}
+                    options={{ webvisor: true }}
+                    version='2'
+                />
+            ) : null}
+            <AppRoutes />
+        </>
+    );
 }
 
 export default App;
